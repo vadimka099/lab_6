76a1d941298065edeff485480b2464971179789b
vadimka099 <47887170+vadimka099@users.noreply.github.com>
2019-04-26 14:52:47 +0300

    Update Students.cpp

--- Students.cpp
+++ Students.cpp
@@ -1,6 +1,6 @@
 #include "Students.h"
 
-const std::string &Student::getName() const {
+const std::string Student::getName() const {
 	return name;
 }
 
@@ -8,7 +8,7 @@ void Student::setName(const std::string &name) {
 	Student::name = name;
 }
 
-const std::string &Student::getSurname() const {
+const std::string Student::getSurname() const {
 	return surname;
 }
 
@@ -16,7 +16,7 @@ void Student::setSurname(const std::string &surname) {
 	Student::surname = surname;
 }
 
-const std::string &Student::getGroup() const {
+const std::string Student::getGroup() const {
 	return group;
 }
 
@@ -24,7 +24,7 @@ void Student::setGroup(const std::string &group) {
 	Student::group = group;
 }
 
-const std::string &Headman::getMail() const {
+const std::string Headman::getMail() const {
 	return mail;
 }
 
@@ -32,7 +32,7 @@ void Headman::setMail(const std::string &mail) {
 	Headman::mail = mail;
 }
 
-const std::string &Headman::getPhoneNumber() const {
+const std::string Headman::getPhoneNumber() const {
 	return phone_number;
 }
 
